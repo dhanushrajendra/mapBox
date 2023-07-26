0eea733ae167190d663333436dae6f8367decf12
dhanushrajendra <dhanush.gr@gmail.com>
2023-07-26 16:24:44 +0530

    intial commit

---
+++ src/Cuboid.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef } from 'react';
+import * as BABYLON from 'babylonjs';
+
+const Cuboid = ({ textureUrl }) => {
+  const canvasRef = useRef(null);
+  const engine = useRef(null);
+  const scene = useRef(null);
+  const camera = useRef(null);
+  const box = useRef(null);
+
+  useEffect(() => {
+    engine.current = new BABYLON.Engine(canvasRef.current, true);
+    scene.current = new BABYLON.Scene(engine.current);
+    camera.current = new BABYLON.ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 5, new BABYLON.Vector3(0, 0, 0), scene.current);
+    camera.current.attachControl(canvasRef.current, true);
+    box.current = BABYLON.MeshBuilder.CreateBox('box', { size: 1 }, scene.current);
+
+    const material = new BABYLON.StandardMaterial('textureMaterial', scene.current);
+    material.diffuseTexture = new BABYLON.Texture(textureUrl, scene.current);
+    box.current.material = material;
+
+    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(1, 1, 0), scene.current);
+
+    engine.current.runRenderLoop(() => {
+      scene.current.render();
+    });
+
+    return () => {
+      scene.current.dispose();
+      engine.current.dispose();
+    };
+  }, [textureUrl]);
+
+  return <canvas ref={canvasRef} style={{ width: '100%', height: '400px' }} />;
+};
+
+export default Cuboid;
---
+++ src/Map.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useRef, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import Cuboid from "./Cuboid";
+
+const Map = () => {
+  const mapContainerRef = useRef(null);
+  const map = useRef(null);
+  const [capturedImage, setCapturedImage] = useState(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZGFubnlyYWplbmRyYTk5IiwiYSI6ImNsa2piYnl1dzAzZzYzdXBjOWljdmMyYzkifQ.Hh0kzUgAfyQfdnkwjsnV7w';
+    map.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [20.5937, 78.9629],
+      zoom: 10,
+    });
+
+    return () => map.current.remove();
+  }, []);
+
+  const handleCapture = () => {
+
+    map.current.once('render', () => {
+      map.current.getCanvas().toBlob((blob) => {
+        setCapturedImage(URL.createObjectURL(blob));
+      });
+    });
+    map.current.triggerRepaint();
+  };
+
+  return (
+    <div>
+      <div ref={mapContainerRef} style={{ width: '100%', height: '400px' }} />
+      <button onClick={handleCapture}>Capture Image</button>
+      {capturedImage && <Cuboid textureUrl={capturedImage} />}
+    </div>
+  );
+};
+
+export default Map;
