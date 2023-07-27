81214dfbcb7aeb776739be24cede7a0cd845da4f
dhanushrajendra <dhanush.gr@gmail.com>
2023-07-27 13:54:51 +0530

    changes

--- src/Cuboid.js
+++ src/Cuboid.js
@@ -9,19 +9,18 @@ const Cuboid = ({ textureUrl }) => {
   const box = useRef(null);
 
   useEffect(() => {
-    
-    if (!engine.current) {
-      engine.current = new BABYLON.Engine(canvasRef.current, true);
-      scene.current = new BABYLON.Scene(engine.current);
-      camera.current = new BABYLON.ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 5, new BABYLON.Vector3(0, 0, 0), scene.current);
-      camera.current.attachControl(canvasRef.current, true);
-      box.current = BABYLON.MeshBuilder.CreateBox('box', { size: 1 }, scene.current);
-    }
+    engine.current = new BABYLON.Engine(canvasRef.current, true);
+    scene.current = new BABYLON.Scene(engine.current);
+    camera.current = new BABYLON.ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 5, new BABYLON.Vector3(0, 0, 0), scene.current);
+    camera.current.attachControl(canvasRef.current, true);
+    box.current = BABYLON.MeshBuilder.CreateBox('box', { size: 1 }, scene.current);
 
-    
     const material = new BABYLON.StandardMaterial('textureMaterial', scene.current);
     material.diffuseTexture = new BABYLON.Texture(textureUrl, scene.current);
     box.current.material = material;
+
+    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(1, 1, 0), scene.current);
+    console.log(light)
 
     engine.current.runRenderLoop(() => {
       scene.current.render();
